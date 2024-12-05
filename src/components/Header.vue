bbbbad22df2013cfe58a9f0553d38ef5e50031af
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2024-12-05 10:17:02 -0300

    feat: add favicon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <header class="max-w-screen-lg mx-auto">
     <nav class="py-6 px-10 xl:px-0 flex justify-between items-center">
       <RouterLink to="/">
-        <img class="h-16" src="/images/logo.png" alt="Tools Invest" />
+        <img width="160px" src="/images/logo.webp" alt="Tools Invest" />
       </RouterLink>
       <ul>
         <li>
