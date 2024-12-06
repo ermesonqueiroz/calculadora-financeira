730ff037e5be8e65d0d9aff4144256328db71888
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2024-12-06 09:38:11 -0300

    feat: add pageview tracker

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import App from './App.vue'
 import { routes } from './router'
 import { ViteSSG } from 'vite-ssg'
-import VueGtag from 'vue-gtag';
+import VueGtag, { pageview } from 'vue-gtag';
 import './style.css'
 
 export const createApp = ViteSSG(
   App,
   { routes },
-  ({ app }) => {
+  ({ app, router }) => {
     app.use(VueGtag, {
       config: {
         id: import.meta.env.VITE_GOOGLE_GTAG_ID
       }
+    });
+    
+    router.beforeEach(({ path: page_path }) => {
+      pageview({ page_path });
     });
   }
 );
